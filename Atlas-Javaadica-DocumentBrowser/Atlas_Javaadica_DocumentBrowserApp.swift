a34079db6b2ef1fa9fb4f5cc82c48fe6ae736a10
Advocates Close <javaad.ali@advocatesclose.nyc>
2024-09-18 22:37:35 -0500

    Initial Commit

---
+++ Atlas-Javaadica-DocumentBrowser/Atlas_Javaadica_DocumentBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Atlas_Javaadica_DocumentBrowserApp.swift
+//  Atlas-Javaadica-DocumentBrowser
+//
+//  Created by AdvocatesClose on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Atlas_Javaadica_DocumentBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
